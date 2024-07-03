ff37e1a923b7904837b89e8ed2758af64dd2d4ce
Yogurt7v <egorov-2k@yandex.ru>
2024-07-03 15:31:04 +0300

    fix: отформатировал код

--- src/singUp.js
+++ src/singUp.js
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useState } from "react";
 import CustomInput from "./components/customInput";
 
 export function SignUp({ Submit }) {
@@ -33,58 +33,56 @@ export function SignUp({ Submit }) {
     <div>
       <h1>SignUp</h1>
       <div className="form">
-
-
-      <form
-        onSubmit={(e) => handleSubmitSignUp(e)}
-        onChange={(e) =>
-          setNewUser({ ...newUser, [e.target.name]: e.target.value })
-        }
-      >
-        <CustomInput
-          type="text"
-          placeholder="Имя"
-          value={newUser.name}
-          name="name"
-          label="Имя"
-        />
-        <CustomInput
-          type="text"
-          placeholder="Никнейм"
-          value={newUser.nickname}
-          name="nickname"
-          label="Никнейм"
-        />
-        <CustomInput
-          type="email"
-          placeholder="Email"
-          value={newUser.email}
-          name="email"
-          label="Email"
-        />
-        <CustomInput
-          type="radio"
-          placeholder="Пол"
-          value={newUser.sex}
-          name="sex"
-          label="Пол"
-        />
-        <CustomInput
-          type="password"
-          placeholder="Пароль"
-          value={newUser.password}
-          name="password"
-          label="Пароль"
-        />
-        <CustomInput
-          type="password"
-          placeholder="Повторите пароль"
-          value={newUser.checkPassword}
-          name="password"
-          label="Повторите пароль"
-        />
-        <CustomInput type="submit" name="submit" value="Submit" />
-      </form>
+        <form
+          onSubmit={(e) => handleSubmitSignUp(e)}
+          onChange={(e) =>
+            setNewUser({ ...newUser, [e.target.name]: e.target.value })
+          }
+        >
+          <CustomInput
+            type="text"
+            placeholder="Имя"
+            value={newUser.name}
+            name="name"
+            label="Имя"
+          />
+          <CustomInput
+            type="text"
+            placeholder="Никнейм"
+            value={newUser.nickname}
+            name="nickname"
+            label="Никнейм"
+          />
+          <CustomInput
+            type="email"
+            placeholder="Email"
+            value={newUser.email}
+            name="email"
+            label="Email"
+          />
+          <CustomInput
+            type="radio"
+            placeholder="Пол"
+            value={newUser.sex}
+            name="sex"
+            label="Пол"
+          />
+          <CustomInput
+            type="password"
+            placeholder="Пароль"
+            value={newUser.password}
+            name="password"
+            label="Пароль"
+          />
+          <CustomInput
+            type="password"
+            placeholder="Повторите пароль"
+            value={newUser.checkPassword}
+            name="password"
+            label="Повторите пароль"
+          />
+          <CustomInput type="submit" name="submit" value="Submit" />
+        </form>
       </div>
     </div>
   );
